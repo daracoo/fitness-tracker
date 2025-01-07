4b1cee117710183650d7afe892aed8694e5687e9
daracoo <dkuzmanoski21@gmail.com>
2025-01-07 16:09:32 +0100

    Improve header on home page

--- lib/pages/home/widgets/header.dart
+++ lib/pages/home/widgets/header.dart
@@ -5,10 +5,84 @@ class AppHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: 200,
-      color: Colors.red[700]
+      child: Stack(
+        children: [
+          CustomPaint(
+            painter: HeaderPainter(),
+            size: Size(double.infinity, 200),
+          ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 35,
+            right: 40,
+            child: CircleAvatar(
+              minRadius: 25,
+              maxRadius: 25,
+              foregroundImage: AssetImage('assets/profile.png'),
+            ),
+          ),
+          Positioned(
+            left: 33,
+            bottom: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Hello', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),),
+                Text('Josh', style: TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.bold),),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+class HeaderPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint backColor = Paint()..color = Color(0xffe20f0f);
+    Paint circles = Paint()..color = Color(0xfff15b5b);
+    canvas.drawRect(Rect.fromPoints(
+        Offset(0,0),
+        Offset(size.width, size.height)
+    ),
+        backColor
+    );
+
+    canvas.drawCircle(
+        Offset(size.width * .65, 10),
+        30,
+        circles
+    );
+    canvas.drawCircle(
+        Offset(size.width * .6, 130),
+        10,
+        circles
+    );
+    canvas.drawCircle(
+        Offset(size.width - 10, size.height - 10),
+        20,
+        circles
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+  
 }
