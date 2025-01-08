6cff47446783dc4d15d43fb1e5e53057856f2a89
daracoo <dkuzmanoski21@gmail.com>
2025-01-08 23:56:17 +0100

    Add days of the week in 'Activity' Screen

--- lib/pages/details/widgets/dates.dart
+++ lib/pages/details/widgets/dates.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_tracker/helpers.dart';
 import 'package:flutter/material.dart';
 
 class Dates extends StatelessWidget {
@@ -5,10 +6,58 @@ class Dates extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<DateBox> dateBoxes = [];
+    
+    DateTime date = DateTime.now();
+    
+    for(int i=0;i<6;i++){
+      dateBoxes.add(DateBox(date: date, active: i == 1,));
+      date = date.add(Duration(days: 1));
+    }
+    
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: dateBoxes,
+      ),
+    );
+  }
+}
+
+class DateBox extends StatelessWidget {
+  final bool active;
+  final DateTime date;
+  const DateBox({
+    super.key, this.active=false, required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 90,
-      color: Colors.lightGreen
+      width: 50,
+      height: 70,
+      decoration: BoxDecoration(
+        gradient: active? LinearGradient(colors: [
+          Color(0xffff92cc),
+          Color(0xfff81e37)
+        ],
+        begin: Alignment.topCenter)
+          : null,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Color(0xffe1e1e1))
+      ),
+      child: DefaultTextStyle.merge(
+        style: active ? TextStyle(color: Colors.white) : null,
+        child: Column(
+          children: [
+            Text(daysOfWeek[date.weekday]!, style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
+            SizedBox(height: 8,),
+            Text(date.day.toString().padLeft(2, '0'), style: TextStyle(fontSize: 27, fontWeight: FontWeight.w500),),
+
+          ],
+        ),
+      ),
     );
   }
 }
