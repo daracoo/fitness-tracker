c71c7ad96946e9dc1aa824b3ef0771082d189deb
daracoo <dkuzmanoski21@gmail.com>
2025-01-08 23:02:56 +0100

    Add switching between programs

--- lib/pages/home/widgets/current.dart
+++ lib/pages/home/widgets/current.dart
@@ -11,6 +11,11 @@ class CurrentPrograms extends StatefulWidget {
 class _CurrentProgramsState extends State<CurrentPrograms> {
   ProgramType active = fitnessPrograms[0].type;
 
+  void _changeProgram(ProgramType newType){
+    setState(() {
+      active = newType;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +42,7 @@ class _CurrentProgramsState extends State<CurrentPrograms> {
                 return Program(
                   program: fitnessPrograms[index],
                   active: fitnessPrograms[index].type == active,
+                  onTap: _changeProgram,
                 );
           },
             separatorBuilder: (context, index) => SizedBox(width: 15,),
@@ -50,52 +56,58 @@ class _CurrentProgramsState extends State<CurrentPrograms> {
 class Program extends StatelessWidget {
   final FitnessProgram program;
   final bool active;
+  final Function(ProgramType) onTap;
 
-  const Program({super.key, required this.program, this.active = false});
+  const Program({super.key, required this.program, this.active = false, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 180,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        image: DecorationImage(
-          colorFilter: ColorFilter.mode(
-              active ?
-              Color(0xfff86767).withOpacity(.8)
-              : Colors.white.withOpacity(.8),
-              BlendMode.lighten
-          ),
-          image: program.image,
-          fit: BoxFit.cover,
-        )
-      ),
-      alignment: Alignment.bottomLeft,
-      padding: const EdgeInsets.all(15),
-      child: DefaultTextStyle.merge(
-        style: TextStyle(
-            color: active ? Colors.white
-            : Colors.black,
-            fontSize: 10,
-            fontWeight: FontWeight.w500
+    return GestureDetector(
+      onTap: () {
+        onTap(program.type);
+      },
+      child: Container(
+        height: 100,
+        width: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+                active ?
+                Color(0xfff86767).withOpacity(.8)
+                : Colors.white.withOpacity(.8),
+                BlendMode.lighten
+            ),
+            image: program.image,
+            fit: BoxFit.cover,
+          )
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(program.name),
-            Row(children: [
-              Text(program.cals),
-              SizedBox(width: 15,),
-              Icon(Icons.timer,
-                size: 10,
-                color: active? Colors.white
-              : Colors.black),
-              SizedBox(width: 5),
-              Text(program.time)
-            ],)
-          ],
+        alignment: Alignment.bottomLeft,
+        padding: const EdgeInsets.all(15),
+        child: DefaultTextStyle.merge(
+          style: TextStyle(
+              color: active ? Colors.white
+              : Colors.black,
+              fontSize: 10,
+              fontWeight: FontWeight.w500
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(program.name),
+              Row(children: [
+                Text(program.cals),
+                SizedBox(width: 15,),
+                Icon(Icons.timer,
+                  size: 10,
+                  color: active? Colors.white
+                : Colors.black),
+                SizedBox(width: 5),
+                Text(program.time)
+              ],)
+            ],
+          ),
         ),
       ),
     );
